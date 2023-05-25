82261748b8dec78cade5dbce021c937adc7853d4
DiazAdriel0 <diazadriel0@gmail.com>
2023-05-25 10:19:44 -0300

    Corrected homework

--- src/selector.js
+++ src/selector.js
@@ -1,26 +1,33 @@
-var traverseDomAndCollectElements = function (matchFunc, startEl) {
-  var resultSet = [];
+let traverseDomAndCollectElements = function (matchFunc, startEl) {
+  let resultSet = [];
   if (typeof startEl === "undefined") startEl = document.body;
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  if (matchFunc(startEl)) resultSet.push(startEl);
+  let elementos = startEl.children
+  if(matchFunc(startEl)) resultSet.push(startEl)
+  for(let i = 0; i < elementos.length; i++){
+    let result = traverseDomAndCollectElements(matchFunc,elementos[i]);
+    resultSet = [...resultSet,...result]
+  }
+  return resultSet
+  /* if (matchFunc(startEl)) resultSet.push(startEl);
   for (let i = 0; i < startEl.children.length; i++) {
     let aux = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
     resultSet = [...resultSet, ...aux];
   }
-  return resultSet;
+  return resultSet; */
 };
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
-var selectorTypeMatcher = function (selector) {
+let selectorTypeMatcher = function (selector) {
   // tu código aquí
-  let firstChar = selector.charAt(0);
-  if(firstChar === "#") return "id";
-  if(firstChar === ".") return "class";
+  //let firstChar = selector.charAt(0);
+  if(selector[0] === "#") return "id";
+  if(selector[0] === ".") return "class";
   else if(selector.includes(".")) return "tag.class";
   else return "tag"
 };
@@ -30,10 +37,28 @@ var selectorTypeMatcher = function (selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function (selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") matchFunction = element => `#${element.id}` === selector;
+let matchFunctionMaker = function (selector) {
+  let selectorType = selectorTypeMatcher(selector);
+  let matchFunction = element => {
+    if (selectorType === "id") return `#${element.id}` === selector;
+    else if (selectorType === "class") {
+      let selectorContent = selector.slice(1)
+      let clases = element.className.split(" ")
+      return (clases.includes(selectorContent))
+    }
+    else if (selectorType === "tag.class") {
+      let [tag,className] = selector.split(".")
+      return (matchFunctionMaker(tag)(element) && matchFunctionMaker(`.${className}`)(element))
+      /*let selectorArray = selector.split(".") 
+      let tagMayus = tag.toUpperCase()
+      let clases = element.className.split(" ")
+      return (element.tagName === tag.toUpperCase() && clases.includes(className)) */
+    }else if (selectorType === "tag") {
+      return (element.tagName === selector.toUpperCase())
+    }
+    return false
+  }
+  /* if (selectorType === "id") matchFunction = element => `#${element.id}` === selector;
   else if (selectorType === "class") {
     matchFunction = element => {
       let selectorContent = selector.slice(1)
@@ -49,13 +74,13 @@ var matchFunctionMaker = function (selector) {
     }
   }else if (selectorType === "tag") {
     matchFunction = element => (element.tagName === selector.toUpperCase())
-  }
+  } */
   return matchFunction;
 };
 
-var $ = function (selector) {
-  var elements;
-  var selectorMatchFunc = matchFunctionMaker(selector);
+let $ = function (selector) {
+  let elements;
+  let selectorMatchFunc = matchFunctionMaker(selector);
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
